7feb7be80979e3ea1fac4ce427d6b2c70ceb3881
Dat Le <dattl@vn.innova.com>
2025-03-21 21:22:45 +0700

    update PORT

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ morgan.token('req_body', function (req, res) {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req_body'));
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 let persons = [
     {
